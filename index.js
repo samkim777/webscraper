d75e0754781f30d2027249562f6cababc8bc60ba
samkim <milk3531@hotmail.com>
2023-06-22 17:20:25 -0700

    cor

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let search_name = '';
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', 'https://webscraper-front.onrender.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
