e8d6dbbe633166aa6872a462f7e581329912d537
samkim <milk3531@hotmail.com>
2023-06-22 19:02:33 -0700

    x requested header

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://webscraper-front.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.headers['x-requested-with'] = 'XMLHttpRequest';
   next();
 });
 
