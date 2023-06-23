6ceaa4f358926e362595151070dd72a63694c2d5
samkim <milk3531@hotmail.com>
2023-06-22 17:42:28 -0700

    a s

--- index.js
+++ index.js
@@ -13,33 +13,12 @@ let search_name = '';
 
 
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
-// Add headers before the routes are defined
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://webscraper-front.onrender.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use(cors({
-  origin: 'https://webscraper-front.onrender.com'
-}));
+  origin: "https://webscraper-front.onrender.com"
+}
+))
+app.options('*', cors())
 
 
 
