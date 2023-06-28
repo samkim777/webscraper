41a6c40a877fa82c4e14f57d39a983b938f8218c
samkim <milk3531@hotmail.com>
2023-06-27 18:35:22 -0700

    cor

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ let search_name = '';
 
 
 
-
-app.use((req, res, next) => {
-  console.log('CORS middleware executed');
-  next();
-});
+app.use(function(req, res, next) {
+      // res.header("Access-Control-Allow-Origin", "*");
+      const allowedOrigins = ['http://localhost:3000', 'https://webscraper-front.onrender.com','http://webscraper-front.onrender.com'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+           res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      res.header("Access-Control-Allow-credentials", true);
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+      next();
+    });
 
 
 
